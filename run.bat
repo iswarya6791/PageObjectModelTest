d54cf977bb40ac896548bd2c7878700d130ab9f9
iswarya <iswarya6791@gmail.com>
2025-07-08 19:35:17 +0530

    "New feature Branch commit"

--- run.bat
+++ run.bat
@@ -1,2 +1,10 @@
 cd C:\Users\Aathvika\eclipse-workspace\PageObjectModelTest
 mvn test
+
+git config --global user.email "you@example.com"
+  git config --global user.name "Your Name"
+git config --global user.email "iswarya6791@gmail.com"
+  git config --global user.name "iswarya"
+
+
+https://github.com/iswarya6791/PageObjectModelTest.git
